1fe7fa9cce171b317ea69ee4d5deb315c5309b1b
jayson_kesterson <your_email@example.com>
2020-04-05 12:39:55 -0400

    Adding fans controller to act as user controller

---
+++ app/controllers/fans_controller.rb
@@ -0,0 +1,25 @@
+class FansController < ApplicationController
+
+    def new
+        @fan = Fan.new
+    end
+
+    def create
+    @fan = Fan.new(fan_params)
+    if @fan.save
+      session[:fan_id] = @fan.id
+      redirect_to fan_path(@fan)
+    else
+      redirect_to fans_path
+    end
+    end
+
+    def show
+    end
+
+    private
+
+    def fan_params
+        params.require(:fan).permit(:username, :password, :email)
+    end
+end
