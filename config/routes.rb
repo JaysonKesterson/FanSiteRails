83838373b18455c9232145f34c362624e3f6f702
jayson_kesterson <your_email@example.com>
2020-04-06 09:45:08 -0400

    adding session routes and resources for team, players and sport

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'fans#index'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
   resources :fans
+
+  resources :teams do
+  resources :players
+  end
+
+  resources :sports
 end
+
