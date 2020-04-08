7e72824ef3249a21e1c7d746f7efb44331ea539a
jayson_kesterson <your_email@example.com>
2020-04-08 10:08:25 -0400

    updating sessions controller for omniauth facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,32 @@ class SessionsController < ApplicationController
       end
     
       def create
-        @fan = Fan.find_by(username: params[:fan][:username])
-        if @fan && @fan.authenticate(params[:fan][:password])
-          session[:fan_id] = @fan.id 
-          redirect_to fan_path(@fan)
+        if params[:fan][:username].present?
+            @fan = Fan.find_by(username: params[:fan][:username])
+            if @fan && @fan.authenticate(params[:fan][:password])
+                session[:fan_id] = @fan.id 
+                redirect_to fan_path(@fan)
+            else
+                redirect_to signin_path
+            end
         else
-          redirect_to signin_path
+            @fan = Fan.find_or_create_by(uid: auth['uid']) do |f|
+                f.username = auth['info']['name']
+                f.email = auth['info']['email']
+            end
+                session[:fan_id] = @fan.id
+                render root_path
         end
       end
     
       def destroy
         session.delete :fan_id
         redirect_to root_path
+      end
+
+      private
+
+      def auth
+        request.env['omniauth.auth']
       end
 end
