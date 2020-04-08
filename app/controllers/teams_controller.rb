2f3a0b7add5b60d537faec8e60a5cbd342254d2f
jayson_kesterson <your_email@example.com>
2020-04-08 13:29:53 -0400

    updating show route for teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,21 @@
 class TeamsController < ApplicationController
+    def new
+        @team = Team.new
+    end
+
+    def create
+        @team = Team.create(team_params)
+        @team.fans << current_fan
+        redirect_to team_path(@team)
+    end
+
+    def show
+        @team = Team.find_by_id(params[:id])
+    end
+
+    private
+
+    def team_params
+        params.require(:team).permit(:teamname, :city, :sport)
+    end
 end
