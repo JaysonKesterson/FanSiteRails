062e4c03b8182c7788e8648559feea64fa6937d8
jayson_kesterson <your_email@example.com>
2020-04-09 18:23:45 -0400

    adjusting signup route from fans/new to '/signup'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'fans#index'
+  get '/signup' => 'fans#new', as: 'signup'
+  post '/signup' => 'fans#create'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/auth/facebook/callback' => 'sessions#create'
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   resources :teams do
   resources :players
   end
+
+  resources :players
 
 end
 
