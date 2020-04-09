ca6c2687c84dbc671c6afe67143a8bd9237d8c53
jayson_kesterson <your_email@example.com>
2020-04-09 18:21:48 -0400

    adding validations to model

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -2,4 +2,7 @@ class Fan < ApplicationRecord
     has_secure_password
     has_many :fan_teams
     has_many :teams, through: :fan_teams
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: {in: 6..20}
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
     belongs_to :team
+    validates :playername, presence: true, uniqueness: true
+    validates :position, presence: true
 end
