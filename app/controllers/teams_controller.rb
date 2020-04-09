9751ce6f4d522aad276676c61dcc4cc17f751216
jayson_kesterson <your_email@example.com>
2020-04-09 18:19:34 -0400

    updating teams controller create action logic to allow to check for validations and errors

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,21 +1,40 @@
 class TeamsController < ApplicationController
     def new
         @team = Team.new
+        @team.fan_teams.build(die_hard_fan: false)
     end
 
     def create
-        @team = Team.create(team_params)
-        @team.fans << current_fan
-        redirect_to team_path(@team)
+        if @team = Team.find_by(teamname: params[:team][:teamname], sport: params[:team][:sport]) 
+            current_fan.teams << @team
+            redirect_to fan_path(current_fan)
+        else
+            binding.pry
+            current_fan.teams.create(team_params)
+            redirect_to fan_path(current_fan)
+        end
     end
 
     def show
         @team = Team.find_by_id(params[:id])
     end
 
+    # def edit
+    #     @team = Team.find_by_id(params[:id])
+    # end
+
+    # def update
+    #     @team = current_fan.teams.find_by_id(params[:id])
+    #     @team.update(team_params)
+    #     redirect_to team_path(@team)
+    # end
+
+    # def delete
+    # end
+
     private
 
     def team_params
-        params.require(:team).permit(:teamname, :city, :sport)
+        params.require(:team).permit(:teamname, :city, :sport, fan_teams_attributes:[:die_hard_fan, :fan_id])
     end
 end
