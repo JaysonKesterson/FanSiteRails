71a3082ede75139a54fe3c4f3f75f2c4e3bc050d
jayson_kesterson <your_email@example.com>
2020-04-16 11:35:56 -0400

    adding helper method to show die_hard_teams and to order teams by fan count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,66 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_fan
-    helper_method :basketball_teams
-    helper_method :baseball_teams
-    helper_method :football_teams
-    helper_method :my_basketball_teams
-    helper_method :my_baseball_teams
-    helper_method :my_football_teams
+    helper_method :current_fan, :logged_in
+    helper_method :basketball_teams, :baseball_teams, :football_teams
+    helper_method :my_basketball_teams, :my_baseball_teams, :my_football_teams, :my_diehard_teams
     
 
     def current_fan
         @current_fan ||= Fan.find_by(id: session[:fan_id])
     end
 
+    def logged_in
+        if !session[:fan_id]
+            redirect_to signin_path
+        end
+    end
+
     def basketball_teams
         @basketball_teams = Team.where(sport: "Basketball")
+        @basketball_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @basketball_teams_ordered = @basketball_teams.order(fancount: :desc)
+    end
+
+    def baseball_teams
+        @baseball_teams = Team.where(sport: "Baseball")
+        @baseball_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @baseball_teams_ordered = @baseball_teams.order(fancount: :desc)
+    end
+
+    def football_teams
+        @football_teams = Team.where(sport: "Football")
+        @football_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @football_teams_ordered = @football_teams.order(fancount: :desc)
     end
 
     def my_basketball_teams
         @my_basketball_teams = current_fan.teams.where(sport: "Basketball")
     end
 
-    def baseball_teams
-        @baseball_teams = Team.where(sport: "Baseball")
-    end
-
     def my_baseball_teams
         @my_baseball_teams = current_fan.teams.where(sport: "Baseball")
     end
 
-    def football_teams
-        @football_teams = Team.where(sport: "Football")
-    end
-
-    
     def my_football_teams
         @my_football_teams = current_fan.teams.where(sport: "Football")
+    end
+
+    def my_diehard_teams
+        @die_hard_teams = []
+        if current_fan 
+            current_fan.fan_teams.where(die_hard_fan: true).each do |team|
+            @team = Team.find_by(id: team.team_id)
+            @die_hard_teams << @team
+            end
+        end
+        @die_hard_teams
     end
 end
