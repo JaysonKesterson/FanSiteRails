f2c6912bfc01c2ce9a9be2974a4e80fa55b7f802
jayson_kesterson <your_email@example.com>
2020-04-16 11:50:39 -0400

    adding validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,8 @@
 class Team < ApplicationRecord
     has_many :players
     has_many :fan_teams
-    accepts_nested_attributes_for :fan_teams
     has_many :fans, through: :fan_teams
+    accepts_nested_attributes_for :fan_teams
     validates :teamname, presence: true
     validates :sport, presence: true
     validates :city, presence: true
