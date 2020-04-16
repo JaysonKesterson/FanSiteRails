f2e18faee1bb35fba29918edf225348e0e5ae842
jayson_kesterson <your_email@example.com>
2020-04-16 11:37:13 -0400

    adding nested new, create, index actions for players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,19 +1,20 @@
 class PlayersController < ApplicationController
+    before_action :logged_in, only:[:create, :new]
     def new
         @player = Player.new(team_id: params[:team_id])
       end
 
     def create
-        @team = Team.find_by(id: params[:player][:team_id])
-        if @player = Player.find_by(playername: params[:player][:playername], position: params[:player][:position])
-            @team.players << @player
-            redirect_to team_players_path(@team)
-        else
-            @player = Player.new(player_params)
-            if @player.valid?
-                @team.players.create(player_params)
+            @team = Team.find_by(id: params[:player][:team_id])
+            if @player = Player.find_by(playername: params[:player][:playername], position: params[:player][:position])
+                @team.players << @player
                 redirect_to team_players_path(@team)
             else
+                @player = Player.new(player_params)
+                if @player.valid?
+                    @team.players.create(player_params)
+                    redirect_to team_players_path(@team)
+                else
                 render :new
             end
         end
