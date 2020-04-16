1d40aaa179a4fa7b5c7ad1d6c57c7f033ac6c974
jayson_kesterson <your_email@example.com>
2020-04-16 11:38:22 -0400

    adding routes for adding an existing team instead of having to always create a new one

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'fans#index'
+  get 'teams/:id/remove' => 'teams#delete', as: 'remove'
+  get '/add_existing_teams' => 'teams#existing_teams'
+  post '/add_existing_teams' => 'teams#add'
   get '/signup' => 'fans#new', as: 'signup'
   post '/signup' => 'fans#create'
   get '/signin' => 'sessions#new'
