246eec0749303766e734061ad5368ab07fb2b1b0
jayson_kesterson <your_email@example.com>
2020-04-16 11:51:09 -0400

    adding validation of scope

--- app/models/fan_team.rb
+++ app/models/fan_team.rb
@@ -1,4 +1,5 @@
 class FanTeam < ApplicationRecord
     belongs_to :fan
     belongs_to :team
+    validates :team_id, uniqueness: { scope: :fan_id, message: "You are already a fan of this team." }
 end
