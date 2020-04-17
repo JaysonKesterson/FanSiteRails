caa79cb7cc13da1afd9daea1dff8e3181185ae38
jayson_kesterson <your_email@example.com>
2020-04-17 13:13:01 -0400

    adding fan teams resources nested inside of teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'fans#index'
-  get 'teams/:id/remove' => 'teams#delete', as: 'remove'
-  get '/add_existing_teams' => 'teams#existing_teams'
-  post '/add_existing_teams' => 'teams#add'
+  # get '/destroy' => 'fanteams#destroy', as: 'fanteams_destroy'
   get '/signup' => 'fans#new', as: 'signup'
   post '/signup' => 'fans#create'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/auth/facebook/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
+
   resources :fans
 
   resources :teams do
+  resources :fanteams
   resources :players
   end
 
