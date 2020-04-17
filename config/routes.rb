7c8e13cea1553e8c0e64bd285afe821c31ae218d
jayson_kesterson <your_email@example.com>
2020-04-17 14:40:43 -0400

    adding patch route for players

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'fans#index'
-  # get '/destroy' => 'fanteams#destroy', as: 'fanteams_destroy'
   get '/signup' => 'fans#new', as: 'signup'
   post '/signup' => 'fans#create'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/auth/facebook/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
-
+  patch '/teams/:team_id/players' => 'players#update'
   resources :fans
 
   resources :teams do
@@ -16,7 +15,7 @@ Rails.application.routes.draw do
   resources :players
   end
 
-  resources :players
+  
 
 end
 
