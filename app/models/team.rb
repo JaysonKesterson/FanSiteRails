d0ca6cb347b847a70875d8b38c3adb5acabf68be
jayson_kesterson <your_email@example.com>
2020-04-25 11:59:46 -0400

    adding helper methods concerning the team model here

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,33 @@ class Team < ApplicationRecord
     validates :teamname, presence: true
     validates :sport, presence: true
     validates :city, presence: true
+
+    def self.basketball_teams
+        @basketball_teams = Team.where(sport: "Basketball")
+        @basketball_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @basketball_teams_ordered = @basketball_teams.order(fancount: :desc)
+    end
+
+    def self.baseball_teams
+        @baseball_teams = Team.where(sport: "Baseball")
+        @baseball_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @baseball_teams_ordered = @baseball_teams.order(fancount: :desc)
+    end
+
+    def self.football_teams
+        @football_teams = Team.where(sport: "Football")
+        @football_teams.each do |team|
+            team.fancount = team.fans.count
+            team.save
+        end
+        @football_teams_ordered = @football_teams.order(fancount: :desc)
+    end
+
+    
 end
