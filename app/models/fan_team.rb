f9a03545a3046ba889b72a44014d20df325ad406
jayson_kesterson <your_email@example.com>
2020-04-27 10:57:27 -0400

    removing validation of scope to prevent error

--- app/models/fan_team.rb
+++ app/models/fan_team.rb
@@ -1,5 +1,4 @@
 class FanTeam < ApplicationRecord
     belongs_to :fan
     belongs_to :team
-    validates :team_id, uniqueness: { scope: :fan_id, message: "You are already a fan of this team." }
 end
