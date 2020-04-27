487af09728a013bb1859280f923ff8a361ff4fbc
jayson_kesterson <your_email@example.com>
2020-04-27 10:56:50 -0400

    updating create route to not allow for a fan to become a fan of a team twice

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,12 +7,18 @@ class TeamsController < ApplicationController
     end
 
     def create
-            @team = Team.new(team_params)
-            if @team.valid?
-                if @team = Team.find_by(teamname: params[:team][:teamname], sport: params[:team][:sport])
-                    current_fan.teams << @team
-                    FanTeam.last.update(die_hard_fan: params[:team][:fan_teams_attributes]["0"]["die_hard_fan"])
-                    redirect_to fan_path(current_fan)
+        @team = Team.find_by(teamname: params[:team][:teamname], sport: params[:team][:sport]) || Team.new(team_params) 
+        if @team.valid?
+                if @team.id != nil
+                    if !current_fan.teams.include?(@team)
+                        current_fan.teams << @team
+                        FanTeam.last.update(die_hard_fan: params[:team][:fan_teams_attributes]["0"]["die_hard_fan"])
+                        redirect_to fan_path(current_fan)
+                    else
+                        @team = Team.new
+                        @team.errors.add(:teamname, ": You are already a fan of this team!")
+                        render :new
+                    end
                 else
                     @team = Team.create(team_params)
                     redirect_to fan_path(current_fan)
@@ -26,38 +32,10 @@ class TeamsController < ApplicationController
         @team = Team.find_by_id(params[:id])
     end
 
-    # def edit
-    #     @team = Team.find_by_id(params[:id])
-    # end
-
-    # def update
-    #     @team = current_fan.teams.find_by_id(params[:id])
-    #     @team.update(team_params)
-    #     redirect_to team_path(@team)
-    # end
-
     def delete
         @fanteam = FanTeam.where(fan_id: current_fan.id, team_id: params[:id])
         current_fan.fan_teams.destroy(@fanteam)
         redirect_to fan_path(current_fan)
-    end
-
-    def existing_teams
-        @team = Team.new
-        @fanteam = FanTeam.new
-        @team.fan_teams.build(die_hard_fan: false)
-    end 
-
-    def add
-        @team = Team.new
-        @fanteam = FanTeam.new(fan_id: params[:team][:fan_teams_attributes]["0"][:fan_id], team_id: params[:team][:id])
-        if @fanteam.save
-        FanTeam.last.update(die_hard_fan: params[:team][:fan_teams_attributes]["0"]["die_hard_fan"])
-        redirect_to fan_path(current_fan)
-        else
-            @team.fan_teams.build(die_hard_fan: false)
-            render :existing_teams
-        end
     end
 
     private
