dd34156a972126ec761acae6a947cb89382caeff
jayson_kesterson <your_email@example.com>
2020-04-27 10:59:17 -0400

    moving methods to appropriate models

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -5,5 +5,4 @@ class Fan < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
-
 end
